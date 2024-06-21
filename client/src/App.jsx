3de4b274e3db96657ad6626ca78b0710508df3c0
VZachary13 <vinicius.zachary@gmail.com>
2024-06-21 15:43:09 -0700

    stuck on login form error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,44 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
-import Navbar from './components/Navbar';
+// Create an HTTP link to the GraphQL server
+const httpLink = createHttpLink({
+  uri: "/graphql", // Adjust the URI as needed
+});
+
+// Create an authentication link to attach the token to each request
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // Return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Create Apollo Client with the authLink and httpLink
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
